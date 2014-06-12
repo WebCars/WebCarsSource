0ea14b37730e7203cc8309c3180c55edb6bace0c
Edian Comachio <ediancomachio@hotmail.com>
2014-06-12 19:21:12 -0300

    Commit de vairas coisas

---
+++ BusinessLayer/AnuncioBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataObjects;
+using DataAccess;
+
+namespace BusinessLayer
+{
+    public class AnuncioBL
+    {
+        public List<global::DataObjects.Anuncio> destaques()
+        {
+            try
+            {
+                return null;
+                //return new AnuncioDA().destaques();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
